d8151c555451bfcbca2b1d40cae2d0a382fec4ec
TomFoolery <TomFoolery@arins-MacBook-Pro.local>
2018-11-15 00:25:38 +0700

    change some endpoint name & add /drivers endpoint

--- backend/app.js
+++ backend/app.js
@@ -14,10 +14,29 @@ app.get('/', (req, res) => {
   res.send('Hiiiii')
 })
 
-app.get('/create', (req, res) => {
+app.get('/all', (req, res) => {
   db.query('SELECT * FROM Route_group', (error, result, fields) => {
-    // console.log(result)
-    res.send(result)
+    if (!error) res.status(200).send(result)
+    else res.status(400).send({ message: 'error' })
+  })
+})
+
+app.post('/create', (req, res) => {
+  const driverID = req.body.ID
+  const destination = req.body.destination
+  db.query(
+    `INSERT INTO Route_group (Destination, Driver_ID) VALUES ('${destination}', '${driverID}')`,
+    (error, result, fields) => {
+      if (!error) res.status(200).send(result)
+      else res.status(400).send({ message: 'error' })
+    }
+  )
+})
+
+app.get('/drivers', (req, res) => {
+  db.query('SELECT ID, Name FROM Driver', (error, result, fields) => {
+    if (!error) res.status(200).send(result)
+    else res.status(400).send({ message: 'error' })
   })
 })
 
