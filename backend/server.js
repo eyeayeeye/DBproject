b28f8fb9ec9bbb039be0587e7fdc94f16a1298cd
TomFoolery <TomFoolery@arins-MacBook-Pro.local>
2018-11-14 18:26:16 +0700

    redo

--- backend/server.js
+++
@@ -1,7 +0,0 @@
-// require('dotenv').config()
-const app = require('./app')
-const port = process.env.PORT || 3002
-
-const server = app.listen(port, () => {
-  console.log('Express server listening on port ' + port)
-})
